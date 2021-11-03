5d00d9d449fee34d033fb7ff431feb0e3f52468b
narumiincode <richardkenji3@outlook.com>
2021-11-03 19:39:18 -0300

    comunicação com o backend

---
+++ Desktop/Generation/Projeto/blog-pessoal/src/paginas/cadastroUsuario/CadastroUsuario.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import './CadastroUsuario.css';
+import 
+
+function CadastroUsuario(){
+    return(<h1> Cadastro de Usuários </h1> )
+}
+
+export default CadastroUsuario;
